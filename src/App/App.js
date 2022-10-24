37845ae9b5c3a9a1e2a5589462eef064ac1c346c
Magdalena Zulewska <madziapernach@gmail.com>
2022-10-24 21:02:33 +0200

    error message added

--- src/App/App.js
+++ src/App/App.js
@@ -8,17 +8,64 @@ import { useState, useEffect } from "react";
 function App() {
   const [rates, setRates] = useState({});
   const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  useEffect(() => {
+  const fetchApi = () => {
     fetch("https://api.exchangerate.host/latest?base=PLN")
+      .then((response) => {
+        if (response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response;
+      })
       .then((response) => response.json())
       .then((dataRates) => {
         setRates(dataRates.rates);
         setDate(dataRates.date);
-      });
+        setLoading(false);
+      })
+      .catch(() => setError(true));
+  };
 
-    console.log(rates);
+  useEffect(() => {
+    setTimeout(fetchApi, 3000);
   }, []);
+
+  if (loading) {
+    return (
+      <>
+        <StyledContainer>
+          <Header content="Kalkulator Walut" />
+          <main>
+            <p>
+              {" "}
+              Ładowanie... Pobieramy aktualne kursy z Banku Centralnego ...
+            </p>
+          </main>
+          <Footer content="Copyright © 2022 - MeggCreative" />
+        </StyledContainer>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <StyledContainer>
+          <Header content="Kalkulator Walut" />
+          <main>
+            <p>
+              {" "}
+              Upssss... Coś poszło nie tak... Sprawdź połączenie internetowe i
+              spróbuj ponownie!
+            </p>
+          </main>
+          <Footer content="Copyright © 2022 - MeggCreative" />
+        </StyledContainer>
+      </>
+    );
+  }
 
   return (
     <>
