181b9445d4ca244a24481ebd9b8fba3b4e4bc451
Magdalena Zulewska <madziapernach@gmail.com>
2022-10-31 08:09:29 +0100

    useCurrentRatesData hook added

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
 import Header from "../Header";
 import Form from "../Form";
 import Footer from "../Footer";
 import Clock from "../Clock";
 import { StyledContainer, StyledParagraph } from "./styled.js";
+import { useCurrentRatesData } from "../useCurrentRatesData";
 
 const FAILURE_TEXT =
   "Upssss... Coś poszło nie tak... Sprawdź połączenie internetowe i spróbuj ponownie!";
@@ -11,33 +11,7 @@ const LOADING_TEXT =
   "Ładowanie... Pobieramy aktualne kursy z Narodowego Banku Polskiego ...";
 
 function App() {
-  const [rates, setRates] = useState({});
-  const [date, setDate] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const fetchApi = async () => {
-    try {
-      const myResponse = await fetch(
-        "https://api.exchangerate.host/latest?base=PLN"
-      );
-      if (!myResponse.ok) {
-        throw new Error(myResponse.statusText);
-      }
-
-      const dataRates = await myResponse.json();
-      setRates(dataRates.rates);
-      setDate(dataRates.date);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    setTimeout(fetchApi, 3000);
-  }, []);
+  const { rates, date, loading, error } = useCurrentRatesData();
 
   const isFinished = !error && !loading;
   return (
